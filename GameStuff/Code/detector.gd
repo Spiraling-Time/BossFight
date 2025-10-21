6621973945ec82098187245f8637fddb5efb927c
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-10-21 17:09:53 -0400

    made cutter work and I can easily change its size and see it

--- GameStuff/Code/detector.gd
+++ GameStuff/Code/detector.gd
@@ -1,9 +1,18 @@
 extends Area2D
-
+@onready var colli = $CollisionShape2D
+@onready var colore = $ColorRect
+func _ready() -> void:
+	colore.size = colli.shape.size
+	colore.position = colli.position - Vector2(colli.shape.size.x/2, colli.shape.size.y/2)
 func _physics_process(delta: float) -> void:
 	for bodies in get_overlapping_bodies():
-		bodies.queue_free()
+		if bodies.modulate != Color.RED:
+			bodies.modulate = Color.RED
 	if Input.is_key_pressed(KEY_LEFT):
 		position.x -= 10
 	elif Input.is_key_pressed(KEY_RIGHT):
 		position.x += 10
+	if Input.is_key_pressed(KEY_UP):
+		position.y -= 10
+	elif Input.is_key_pressed(KEY_DOWN):
+		position.y += 10
--- GameStuff/Code/world.gd
+++ GameStuff/Code/world.gd
@@ -1,19 +1,14 @@
 extends Node2D
-
-@export var block = preload("res://GameStuff/Scenes/block.tscn")
-
-var gridX: int = 50
-var gridY: int = 50
-var size: int = 10
-var offset: Vector2 = Vector2(gridX*size/2, gridY*size/2)
-
+#@export var block = preload("res://GameStuff/Scenes/block.tscn")
+#var gridX: int = 50
+#var gridY: int = 50
+#var size: int = 10
+#var offset: Vector2 = Vector2(gridX*size/2, gridY*size/2)
 func _ready() -> void:
-	"""Create Grid:"""
+	#CREATE GRID
 	#for i in range(gridX):
 		#for j in range(gridY):
 			#var newBlock = block.instantiate()
 			#newBlock.position = Vector2(i*size,j*size)-offset
 			#add_child(newBlock)
-			
-			
 	pass
