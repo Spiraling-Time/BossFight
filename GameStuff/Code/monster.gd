ecc9822fd622805242bbbfb6ddb262c6b16e1741
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-10-21 17:37:39 -0400

    made a reset button

---
+++ GameStuff/Code/monster.gd
@@ -0,0 +1,13 @@
+extends Node2D
+var ogMon
+var mon
+func _ready() -> void:
+	ogMon = $Maps
+	mon = ogMon.duplicate()
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_Z):
+		ogMon.queue_free()
+		var newMon = mon.duplicate()
+		add_child(newMon)
+		ogMon = newMon
