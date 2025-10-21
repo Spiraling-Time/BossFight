6ea5bbe0dcf0c79c42becfd28fbed9ab773eac9c
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-10-21 18:21:26 -0400

    made it so after hitting tiles they can't be hit more (setting up pathfinding)

--- GameStuff/Code/detector.gd
+++ GameStuff/Code/detector.gd
@@ -9,6 +9,7 @@ func _physics_process(delta: float) -> void:
 		if bodies.modulate != Color.RED:
 			bodies.visible = true
 			bodies.modulate = Color.RED
+			bodies.set_collision_layer_value(1, false,)
 	if Input.is_key_pressed(KEY_LEFT):
 		position.x -= 10
 	elif Input.is_key_pressed(KEY_RIGHT):
