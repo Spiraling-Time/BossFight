5fe7882c05d0cbe19bea2850fb6ccc8e17b53627
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-10-22 15:41:04 -0400

    starting the weird pathfinding thing I want to do

--- GameStuff/Code/monster.gd
+++ GameStuff/Code/monster.gd
@@ -2,7 +2,7 @@ extends Node2D
 var ogMon
 var mon
 func _ready() -> void:
-	ogMon = $Maps
+	ogMon = $Limb/Maps
 	mon = ogMon.duplicate()
 	
 func _physics_process(delta: float) -> void:
@@ -11,3 +11,10 @@ func _physics_process(delta: float) -> void:
 		var newMon = mon.duplicate()
 		add_child(newMon)
 		ogMon = newMon
+
+
+func _on_check_wounds_timeout() -> void:
+	ogMon.queue_free()
+	var newMon = mon.duplicate()
+	add_child(newMon)
+	ogMon = newMon
