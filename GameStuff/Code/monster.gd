73ecc8f7a51efb936aa83eb32d8696311ca5753a
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-10-28 19:39:17 -0400

    made hitboxes (hopefully corners are fine)

--- GameStuff/Code/monster.gd
+++ GameStuff/Code/monster.gd
@@ -4,7 +4,6 @@ func _ready() -> void:
 	pass
 func _on_check_wounds_timeout() -> void:
 	limb1.wounds_timeout()
-	$NavigationAgent2D.target_position = Vector2(0,-500)
-	print($NavigationAgent2D.get_next_path_position())
+
 func _physics_process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_Z): _on_check_wounds_timeout()
