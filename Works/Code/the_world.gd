ce6854f0d40690afa3377a2b49025ed3e7690b7a
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-11-30 12:34:02 -0500

    perfect!

--- Works/Code/the_world.gd
+++ Works/Code/the_world.gd
@@ -1,6 +1,6 @@
 extends Node2D
-var gridX: int = 1920/4
-var gridY: int = 1080/4
+var gridX: int = 480#150#1920/4
+var gridY: int = 270#250#1080/4
 var size: int = 5
 var offset: Vector2 = Vector2(gridX*size/2, gridY*size/2)
 var points: Array = []
@@ -27,30 +27,44 @@ func _draw() -> void:
 	for i in range(points.size()):
 		draw_circle(points[i][0], points[i][1], points[i][2])
 
+func in_main_part(i, j):
+	if (i % 2 == 0 && i > gridX/2 -65 && i < gridX/2 +65) && (j % 2 == 0 && j > gridY/2 -100 && j < gridY/2 +100): return true
+	else: return false
 func generate_stars():
 	for i in range(gridX):
-		if i % size == 0:
+		#if i % 5 == 0: #size
 			for j in range(gridY):
-				if j % size == 0:
+				#if j % 5 == 0: #size
 					var po = Vector2(i*size,j*size)-offset +Vector2(randi_range(-10, 10), randi_range(-10, 10))#Vector2(i*size,j*size)-offset + Vector2(randi_range(-1*(size*10), (size*10)), randi_range(-1*(size*10), (size*10)))
 					var starSize = randi_range(-1, 1)+size/2
 
 #TRIDENT
-					if Geometry2D.is_point_in_polygon(po, poly9.polygon): points.append([po, starSize+2, Color(0.0, randi_range(2,6)/10.0, 15.723, 1.0), true, "trident"])
+					if Geometry2D.is_point_in_polygon(po, poly9.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, randi_range(2,6)/10.0, 15.723, 1.0), true, "trident"])
 #PANTS
-					elif Geometry2D.is_point_in_polygon(po, poly7.polygon): points.append([po, starSize+2, Color(0.0, randi_range(1,5)/10.0, 0.0, 1.0), true, "pants"])
+					elif Geometry2D.is_point_in_polygon(po, poly7.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, randi_range(1,5)/10.0, 0.0, 1.0), true, "pants"])
 #BODY-TORSO%FACE
-					elif Geometry2D.is_point_in_polygon(po, poly6.polygon): points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
-					elif Geometry2D.is_point_in_polygon(po, poly5.polygon): points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
-					elif Geometry2D.is_point_in_polygon(po, poly4.polygon): points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
+					elif Geometry2D.is_point_in_polygon(po, poly6.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
+					elif Geometry2D.is_point_in_polygon(po, poly5.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
+					elif Geometry2D.is_point_in_polygon(po, poly4.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
 #BEARD&FACE
-					elif Geometry2D.is_point_in_polygon(po, poly2.polygon): points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
-					elif Geometry2D.is_point_in_polygon(po, poly8.polygon): points.append([po, starSize+2, Color(0.357, randi_range(2,5)/10.0, 0.0, 1.0), true, "beard"])
+					elif Geometry2D.is_point_in_polygon(po, poly2.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
+					elif Geometry2D.is_point_in_polygon(po, poly8.polygon) && in_main_part(i,j):
+						points.append([po, starSize+2, Color(0.357, randi_range(2,5)/10.0, 0.0, 1.0), true, "beard"])
 #TORSO
-					elif Geometry2D.is_point_in_polygon(po, poly3.polygon): points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
+					elif Geometry2D.is_point_in_polygon(po, poly3.polygon) && in_main_part(i,j): 
+						points.append([po, starSize+2, Color(0.0, 0.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0), true, "body"])
 #BACKGROUND
-					elif Geometry2D.is_point_in_polygon(po, poly1.polygon): points.append([po, starSize, Color(randi_range(5,10)/10.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0, 1.0), true, "backward"])
+					elif Geometry2D.is_point_in_polygon(po, poly1.polygon) && i % 5 == 0 && j % 5 == 0:
+						points.append([po, starSize, Color(randi_range(5,10)/10.0, randi_range(5,10)/10.0, randi_range(5,10)/10.0, 1.0), true, "backward"])
 	timer.start()
+
+
 func _on_timer_timeout() -> void:
 	for j in range(points.size()):
 		if points[j][4] == "backward":
