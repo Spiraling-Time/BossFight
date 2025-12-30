e59f237bb76b092f3f09478e4385f1f407f68e9b
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-12-30 15:29:40 -0500

    AGHAHAHAHAHAHAHAHAHA IT WORKS IT WORKS AAHAHAHAHAHA

--- even_more_experiment.gd
+++ even_more_experiment.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 
-var gridX: int = 1920/60#480#150#1920/4
-var gridY: int = 1080/60#270#250#1080/4
+var gridX: int = 1920/45#480#150#1920/4
+var gridY: int = 1080/45#270#250#1080/4
 var size: float = 0.002
 var fakeCenter = Vector2(1920/1080*.5, 1920/1080*.5)
 var center = fakeCenter*Vector2(1.0, 0.555)
@@ -16,12 +16,11 @@ func _ready() -> void:
 	generate_stars()
 	#print(points)
 	r.set_shader_parameter("circles", points)
-#func _draw() -> void:
-	#for i in range(points.size()):
-		#draw_circle(points[i][0], points[i][1], points[i][2])
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("up"):
+	if Input.is_action_just_pressed("up"):
+		points.clear()
+		generate_stars()
 		r.set_shader_parameter("circles", points)
 
 func generate_stars():
@@ -32,4 +31,4 @@ func generate_stars():
 					#var po = Vector2(i*size,j*size)-offset +Vector2(randi_range(-10, 10), randi_range(-10, 10))#Vector2(i*size,j*size)-offset + Vector2(randi_range(-1*(size*10), (size*10)), randi_range(-1*(size*10), (size*10)))
 					#var starSize = 2+randi_range(-1, 1)+size/2
 					#points.append(Vector3(center.x*(i*0.01-offset.x), center.y*(j*0.01-offset.y), 0.01))
-					points.append(Vector3(i*offsize-offset.x+center.x, j*offsize-offset.y+center.y,size+randi_range(-1,1)*size/20))
+					points.append(Vector3(i*offsize-offset.x+center.x+(offsize*randi_range(-2, 2)), j*offsize-offset.y+center.y+(offsize*randi_range(-2, 2)), size+randi_range(-1,1)*size/20))
