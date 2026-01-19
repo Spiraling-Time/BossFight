f60671f8fade3884a5e2a0f4e31e3a157463e0b0
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2026-01-18 19:13:23 -0500

    stars done!

--- even_more_experiment.gd
+++ even_more_experiment.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-var gridX: int = 1920/40#480#150#1920/4
-var gridY: int = 1080/54#270#250#1080/4
+var gridX: int = 1080/36#480#150#1920/4
+var gridY: int = 1080/36#270#250#1080/4
 var size: float = 0.002
 var points: Array = []# = [Vector3(fakeCenter.x, fakeCenter.y*0.555 ,0.001)]
 var colors: Array = []
-var points2: Array = []
-var colors2: Array = []
+
 
 @onready var r = $CanvasLayer/rectangle.material
-@onready var r2 = $CanvasLayer2/rectangle.material
 
 
 func _ready() -> void:
@@ -18,22 +16,16 @@ func _ready() -> void:
 	#print(points)
 	r.set_shader_parameter("circles", points)
 	r.set_shader_parameter("colors", colors)
-	r2.set_shader_parameter("circles", points2)
-	r2.set_shader_parameter("colors", colors2)
+
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("up"):
 		points.clear()
 		colors.clear()
-		points2.clear()
-		colors2.clear()
 		generate_stars()
 		r.set_shader_parameter("circles", points)
 		r.set_shader_parameter("colors", colors)
-		r2.set_shader_parameter("circles", points2)
-		r2.set_shader_parameter("colors", colors2)
-	elif Input.is_action_just_pressed("down"):
-		pass
+
 
 #
 #func generate_stars():
@@ -58,6 +50,3 @@ func generate_stars():
 		for j in range(gridY):
 			points.append(Vector3((i + 0.5) * stepX, (j + 0.5) * stepY, size)+Vector3(stepX*randf_range(0.5,-0.5), stepY*randf_range(0.5,-0.5), 0.0))
 			colors.append(Vector3(randi_range(5,10) / 10.0, randi_range(5,10) / 10.0, randi_range(5,10) / 10.0))
-
-			points2.append(Vector3((i + 0.5) * stepX, (j + 0.5) * stepY, size)+Vector3(stepX*randf_range(0.5,-0.5), stepY*randf_range(0.5,-0.5), 0.0))
-			colors2.append(Vector3(randi_range(5,10) / 10.0, randi_range(5,10) / 10.0, randi_range(5,10) / 10.0))
